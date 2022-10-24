4ec97dae64b4369765aae2f4d9852a35877517de
Melina Alegre <alegre.melina7@gmail.com>
2022-10-24 13:21:50 -0300

    desafio 2

---
+++ index.js
@@ -0,0 +1,32 @@
+const Contenedor = require("./contenedor")
+const contenedor = new Contenedor()
+
+    let product ={
+        title: "Manga Dorohedoro 1",
+        price: 2000,
+        thumbnail:"https://http2.mlstatic.com/D_NQ_NP_691062-MLA51760448918_092022-O.webp",
+    }
+
+//Crea un nuevo producto y lo guarda en el JSON
+ contenedor.save(product).then((response) =>{
+       console.log(response)
+    })
+
+//Muestra todos los productos
+// contenedor.getAll().then((response) =>{
+//     console.log(response)
+//  })
+
+//Muestra un producto mediante su ID
+//  contenedor.getById(2).then((response) =>{
+//      console.log(response);
+//      })
+
+//Elimina un producto mediante su ID
+//  contenedor.deleteById(2).then((response) => {
+//      console.log(response);
+//      });
+
+//Elimina todos los productos
+//contenedor.deleteAll()
+
